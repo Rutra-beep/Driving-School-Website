7cc2329127d87ef988a17205072081a3b4a72d50
Artur Kaceli <arturkaceli15@gmail.com>
2022-12-09 23:12:08 +0100

    'nice work done'

--- script/call.js
+++ script/call.js
@@ -5,10 +5,18 @@ let close_btn = document.querySelector("#closebtn");
 let hidden = document.querySelector(".call__hidden");
 let user = document.querySelector(".user");
 let arrow = document.querySelector(".arrow");
+let hidden_socials = document.querySelector('.call__hidden__socials');
 
+// Open Button(Phone icon) 
 openbtn.addEventListener("click", () => {
   call.style.width = "270px";
-  call.style.height = "220px";
+  if (hidden_socials.classList.contains('active')) {
+    call.classList.toggle('active-300')
+    call.classList.remove('active-220')
+  }
+  else{
+    call.classList.toggle('active-220')
+  }
   phone_icon.style.display = "none";
   hidden.style.display = "flex";
   setTimeout(() => {
@@ -20,8 +28,11 @@ openbtn.addEventListener("click", () => {
   openbtn.style.display = "none";
 });
 
+// Close Button
 close_btn.addEventListener("click", () => {
-  call.style.height = "80px";
+  call.classList.remove('active-220')
+  call.classList.remove('active-300')
+  call.classList.toggle('active-80')
   setTimeout(() => {
     call.style.width = "80px";
   }, 80);
@@ -30,3 +41,13 @@ close_btn.addEventListener("click", () => {
   user.style.opacity = "0";
   arrow.style.opacity = "0";
 });
+
+// Arrow
+arrow.addEventListener('click', ()=>{
+  console.log('arrow');
+  hidden_socials.classList.toggle('active')
+  call.classList.toggle('active-300')
+  call.classList.toggle('active-220')
+
+  arrow.classList.toggle('rotate')
+})
